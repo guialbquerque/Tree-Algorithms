33769be52d0dfe613829486acae40b22a41eaaca
guialbquerque <guialbquerque@gmail.com>
2022-08-08 13:20:35 -0300

    Insert method created, in a binary tree all elements less than or equal to the root are located in left, and the elements greater or equal are located in the right of the root, the parents and the children are created made this process since the root until the last leaf, when the parent.left or parent.right is None

--- Binary-Tree.py
+++ Binary-Tree.py
@@ -13,3 +13,25 @@ class BinaryTreeSearch:
         self.size = 0
     def __len__(self):
         return self.size
+
+    def insert(self, value):
+        new = Node(value)
+        if self.size == 0:
+            self.root = new
+            self.size += 1
+        else:
+            actual = self.root
+            while True:
+                parent = actual
+                if value < actual.value:
+                    actual = actual.left
+                    if actual == None:
+                        parent.left = new
+                    return
+                else:
+                    actual = actual.right
+                    if actual == None:
+                        parent.right = new
+                        return
+        self.size += 1
+
