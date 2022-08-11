ffc8edc021b19024f35e49bab033049bec343fd9
guialbquerque <guialbquerque@gmail.com>
2022-08-11 20:06:03 -0300

    Pre order crossing method was created, its a recursive method which walking throug all elementos of the tree first initially in the node chosen and after that, make a recursive call in node.left and node.right of the respective node.

--- Binary-Tree.py
+++ Binary-Tree.py
@@ -53,6 +53,12 @@ class BinaryTreeSearch:
                     return None
         return actual
 
+    def pre_order_crossing(self, node):
+        if node != None:
+            print(node.value)
+            self.pre_order_crossing(node.left)
+            self.pre_order_crossing(node.right)
+
 if __name__ == "__main__":                
 
 
@@ -78,3 +84,5 @@ if __name__ == "__main__":
     print(BT.search(10))
     print(BT.search(84))
     print(BT.search(100))
+    print("--------------------------------")
+    BT.pre_order_crossing(BT.root)
